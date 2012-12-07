6ce4d561d8e276a6de03f060baec8547b3bcd07d
arlg <gantier.aurelien@gmail.com>
2012-12-07 01:50:13 +0000

    Final Elodie

--- www/6/js/main.js
+++ www/6/js/main.js
@@ -1,3 +1,19 @@
+/*
+* 18 Days Left - Elodie Fabbri
+* @author Aurelien Gantier http://www.arlg.me
+* Loads images and show them like a gif
+                                                       _v
+                                                     __* (__)
+             ff     ff     ff     ff                {\/ (_(__).-.
+      ff    <_\__, <_\__, <_\__, <_\__,      __,~~.(`>|-(___)/ ,_)
+    o<_\__,  (_ ff ~(_ ff ~(_ ff ~(_ ff~~~~~@ )\/_;-"``     |
+      (___)~~//<_\__, <_\__, <_\__, <_\__,    | \__________/|
+      // >>     (___)~~(___)~~(___)~~(___)~~~~\\_/_______\_//
+                // >>  // >>  // >>  // >> jgs `'---------'`
+
+*/
+
+
 var APP = window.APP || {};
 
 APP.Main = {
@@ -10,21 +26,26 @@ APP.Main = {
 	canvas: document.getElementById("canvas"),
 	CANVAS_WIDTH: 1000,
 	CANVAS_HEIGHT: 665,
+	FPS : 5,
 	ctx:'',
-	current : 1,
+	current : 0,
+	preloaded : 0,
+	preloadedId : document.getElementById("number"),
+	preloadedWrapper : document.getElementById("preload"),
+
 
 	init: function() {
 		APP.Main.initCanvas();
 		APP.Main.loadImages();
-		
-		setTimeout(APP.Main.loop, 3000);
-		//this.ctx.drawImage(this.images[0], 0, 0);
 	},
 
 	loadImages: function() {
 		for(var i = 0; i < APP.Main.imagesLength; i++) {
 			var image = new Image();
 			image.src = APP.Main.imagesPath + (i + 1) + APP.Main.imagesExt;
+			image.onload = function() {
+				APP.Main.onLoaded();
+			};
 			APP.Main.images[i] = image;
 		}
 	},
@@ -38,30 +59,27 @@ APP.Main = {
 		//this.loop();
 	},
 
+	onLoaded: function(){
+		APP.Main.preloaded++;
+		APP.Main.preloadedId.innerHTML = APP.Main.preloaded;
+		if(APP.Main.preloaded == APP.Main.imagesLength){
+			APP.Main.loop();
+			APP.Main.preloadedWrapper.style.visibility = 'hidden';
+		}
+	},
+
 	loop : function(){
 		//Clear
 		APP.Main.ctx.clearRect( 0, 0, APP.Main.CANVAS_WIDTH, APP.Main.CANVAS_HEIGHT );
 
-		//update
-
 		if(APP.Main.current == APP.Main.imagesLength) APP.Main.current = 0;
-
 		APP.Main.ctx.drawImage(APP.Main.images[APP.Main.current], 0, 0);
 		APP.Main.current++;
 
-		console.log(APP.Main.current);
-
-
-		//draw
-
 		//next
-		//requestAnimFrame(APP.Main.loop);
-		setTimeout(APP.Main.loop, 1000 / 5);
-
+		setTimeout(APP.Main.loop, 1000 / APP.Main.FPS);
 	}
 
 };
 
-
 APP.Main.init();
-
