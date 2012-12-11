fa35c88ced77b09fdfa117e6b980d68edaa7bbae
dmmn <ddotmnn+git@gmail.com>
2012-12-11 02:23:05 +0000

    add mobile ribbon and chrome only message

--- www/js/prod/browserTester.js
+++ www/js/prod/browserTester.js
@@ -1,7 +1,7 @@
 (function () {
   $('#calendar .entry').each(function () {
-    if(!$.browser.chrome) {
-      $(this).find('.tooltip').html('Sorry this experiment only works on <a href="http://google.com/chrome">Chrome</a>');
+    if(!$.browser.chrome && $(this).hasClass('chromeOnly')) {
+      $(this).find('.tooltip').html('SORRY YOU NEED GOOGLE CHROME TO SEE THIS EXPERIMENT');
       $(this).find('a').attr('href', "").css('cursor', 'default');
       $(this).click(function() { return false; });
     }
