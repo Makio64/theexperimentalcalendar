ca8f472cc51edfd319749efdb6f6bf4cc75d8224
Victor Pamelard <victor.pamelard@gmail.com>
2012-12-17 21:16:15 +0100

    add expe 18

---
+++ www/18/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!--
+        Hello and welcome in the source code of
+        The Christmas Experiments Project
+        - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        - The sources are avaible on GitHub : https://github.com/ipfix/theexperimentalcalendar
+           *             ,
+                       _/^\_
+                      <     >
+     *                 /.-.\         *
+              *        `/&\`                   *
+                      ,@.*;@,
+                     /_o.I %_\    *
+        *           (`'--:o(_@;
+                   /`;--.,__ `')             *
+                  ;@`o % O,*`'`&\ 
+            *    (`'--)_@ ;o %'()\      *
+                 /`;--._`''--._O'@;
+                /&*,()~o`;-.,_ `""`)
+     *          /`,@ ;+& () o*`;-';\
+               (`""--.,_0 +% @' &()\
+               /-.,_    ``''--....-'`)  *
+          *    /@%;o`:;'--,.__   __.'\
+              ;*,&(); @ % &^;~`"`o;@();         *
+              /(); o^~; & ().o@*&`;&%O\
+              `"="==""==,,,.,="=="==="`
+           __.----.(\-''#####---...___...-----._
+         '`         \)_`"""""`
+                 .--' ')
+               o(  )_-\
+                 `"""` `
+    -->
+    <head>
+        <title>The Christmas Experiments - SNOCKS</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Victor Pamelard">
+        <meta name="description" content="SNOCKS made with love by Victor Pamelard for The Christmas Experiments.">
+        <meta property="og:title" content="The Christmas Experiments - SNOCKS"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="http://christmasexperiments.com/18/"/>
+        <meta property="og:image" content="http://christmasexperiments.com/img/facebook.jpg"/>
+        <meta property="og:site_name" content="The Christmas Experiments - SNOCKS"/>
+        <meta property="og:description" content="SNOCKS made with love by Victor Pamelard for The Christmas Experiments."/>
+        <link rel="Shortcut Icon" type="image/ico" href="../img/favicon.png" />
+        <link rel="Shortcut Icon" type="image/png" href="../img/favicon.png" />
+        <link rel="stylesheet" href="../css/reset.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="../css/content.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="../css/header.css" type="text/css" media="screen">
+    </head>
+    <body style="overflow:hidden">
+        <header style="border-bottom:solid 1px #EEE">
+            <div class="center">
+                <h1><a href="/" title="The Christmas Experiments - Home">TCE</a></h1>
+                <h2>
+                    <a href="http://victorpamelard.com/" target='_blank'>
+                        Day 18 - SNOCKS by Victor Pamelard
+                    </a>
+                </h2>
+                <ul id="share">
+                    <li id="google"><a href="/" title="Share this Christmas Experiment on google+">G</a></li>
+                    <li id="twitter"><a href="/" title="Share this Christmas Experiment on twitter">T</a></li> 
+                    <li id="facebook"><a href="/" title="Share this Christmas Experiment on facebook">F</a></li>                   
+                </ul>
+            </div>
+        </header>
+        <iframe style="top:50px" src="http://88.190.47.89/victor_snocks/public/<?php if(isset($_GET["room"])) echo "?room=" . $_GET["room"];?>"></iframe>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+        <script type="text/javascript" src="../js/header.js"></script>
+                <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36772924-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+    </body>
+</html>
